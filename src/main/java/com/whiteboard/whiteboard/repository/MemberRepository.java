2f667c30e6d2eb5c2133444d3fdb1fc4139f55d2
고니 <chansol91@naver.com>
2023-09-12 12:49:17 +0900

    암호

--- src/main/java/com/whiteboard/whiteboard/repository/MemberRepository.java
+++ src/main/java/com/whiteboard/whiteboard/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.whiteboard.whiteboard.entity.Member;
 
+
 public interface MemberRepository extends JpaRepository<Member,String>{
     
 }
