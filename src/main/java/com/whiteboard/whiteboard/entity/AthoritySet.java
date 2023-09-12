e1448070423d29c606de0f2416e063f5cba61cdc
고니 <chansol91@naver.com>
2023-09-12 11:02:11 +0900

    회원가입DB Test

--- src/main/java/com/whiteboard/whiteboard/entity/AthoritySet.java
+++ src/main/java/com/whiteboard/whiteboard/entity/AthoritySet.java
@@ -1,3 +1,4 @@
+package com.whiteboard.whiteboard.entity;
 public enum AthoritySet {
 
   MEMBER, ADMIN; //권한 종류 : 회원 & 관리자
--- src/test/java/com/whiteboard/whiteboard/WhiteboardApplicationTests.java
+++ src/test/java/com/whiteboard/whiteboard/WhiteboardApplicationTests.java
@@ -1,13 +1,37 @@
 package com.whiteboard.whiteboard;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.whiteboard.whiteboard.entity.Member;
+import com.whiteboard.whiteboard.repository.MemberRepository;
 
 @SpringBootTest
 class WhiteboardApplicationTests {
 
+	@Autowired
+	private MemberRepository memberRepository;//멤버 레포지토리
+
 	@Test
-	void contextLoads() {
+	public void memberregister() {
+
+		Member member = Member.builder()
+						.id("mingun91@naver.com")
+						.password("7777")
+						.phoneNum("01094800129")
+						.nickname("민건왕자")
+						.gender("하남자")
+						.birthDay(LocalDate.parse("1991-05-02"))
+						.isSns(false)
+						.build();
+		memberRepository.save(member);
+
+		System.err.println(member);
 	}
+
+	
 
 }
