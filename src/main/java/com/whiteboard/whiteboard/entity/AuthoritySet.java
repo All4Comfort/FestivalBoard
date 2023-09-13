8d3f715e7d8e7eddc165a62942b35ee961f0b8f3
한울 <bluemoon_sound@naver.com>
2023-09-13 10:57:56 +0900

    한울

---
+++ src/main/java/com/whiteboard/whiteboard/entity/AuthoritySet.java
@@ -0,0 +1,7 @@
+package com.whiteboard.whiteboard.entity;
+
+public enum AuthoritySet {
+
+  MEMBER, ADMIN; //권한 종류 : 회원 & 관리자....회원가입한 사람의 데이터이므로 all은 불가능
+  
+}
--- src/main/java/com/whiteboard/whiteboard/entity/Member.java
+++ src/main/java/com/whiteboard/whiteboard/entity/Member.java
@@ -1,9 +1,13 @@
 package com.whiteboard.whiteboard.entity;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,7 +29,7 @@ public class Member extends BaseEntity{
 	@Id
 	private String id; //아이디 : 이메일주소로 받기
 	
-	private String password; //비밀번호 : 암호화해줘야 함
+	private String pw; //비밀번호 : 암호화해줘야 함
 
 	@Column(nullable = false, length = 10)
 	private String name; //실명
@@ -45,9 +49,9 @@ public class Member extends BaseEntity{
 	private boolean isSns; //소셜계정 인증을 통한 가입 여부
 	
 	// 여기에 비밀번호 업데이트 로직을 추가할 수 있음
-	public void updatePassword(String newPassword) {
+	public void updatepw(String newPW) {
 
-		this.password = newPassword;
+		this.pw = newPW;
 }
 
 // 실명 업데이트 메서드
@@ -67,4 +71,18 @@ public void updatePhoneNum(String newPhoneNum) {
 		// 여기에 전화번호 업데이트 로직을 추가할 수 있음
 		this.phoneNum = newPhoneNum;
 }
+
+//회원이 DB에 인서트(생성)될 때 권한 부여하기
+
+//권한 엔티티를 제네릭으로 준 컬랙션 생성
+@Builder.Default
+//아래 어노테이션은 자동으로 참조를 걸어줌
+@ElementCollection(fetch = FetchType.LAZY) //권한값을 항상 가져올 텐데 LAZY가 꼭 필요한가..? 추후 수정할 수도...
+private Set<AuthoritySet> authority = new HashSet<>(); 
+
+//회원이 DB에 인서트(생성) 시 권한 부여 메서드
+public void addAuthority(AuthoritySet authoritySet){
+	authority.add(authoritySet);
+}
+
 }
