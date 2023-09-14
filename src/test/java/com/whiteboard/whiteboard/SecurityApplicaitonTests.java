687c67bf7614035667bab73f5e568861c84a28cb
고니 <chansol91@naver.com>
2023-09-14 09:49:46 +0900

    수정

--- src/test/java/com/whiteboard/whiteboard/SecurityApplicaitonTests.java
+++ src/test/java/com/whiteboard/whiteboard/SecurityApplicaitonTests.java
@@ -1,15 +1,9 @@
 package com.whiteboard.whiteboard;
 
-import java.time.LocalDate;
-import java.util.stream.IntStream;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.whiteboard.whiteboard.entity.AuthoritySet;
-import com.whiteboard.whiteboard.entity.Member;
 import com.whiteboard.whiteboard.repository.MemberRepository;
 
 @SpringBootTest
@@ -19,28 +13,28 @@ class SecurityApplicaitonTests {
   private PasswordEncoder passwordEncoder;
   @Autowired
 	private MemberRepository memberRepository;
-	@Test
-  void contextLoads() {
-    IntStream.rangeClosed(2, 10).forEach(i -> 
-    {Member member = Member.builder().
-                    email("회원" + i).
-                    pw(passwordEncoder.encode("1234")).
-                    name("이름" + i).
-                    phoneNum("1234567890" + i).
-                    nickname("닉네임" + i).
-                    birthDay(LocalDate.parse("2001-11-11")).
-                    gender("여자").
-                    isSns(false).
-                    build();
+	//@Test
+  // void contextLoads() {
+  //   IntStream.rangeClosed(2, 10).forEach(i -> 
+  //   {Member member = Member.builder().
+  //                   email("회원" + i).
+  //                   pw(passwordEncoder.encode("1234")).
+  //                   name("이름" + i).
+  //                   phoneNum("1234567890" + i).
+  //                   nickname("닉네임" + i).
+  //                   birthDay(LocalDate.parse("2001-11-11")).
+  //                   gender("여자").
+  //                   isSns(false).
+  //                   build();
                     
-    member.addAuthority(AuthoritySet.MEMBER);
+  //   member.addAuthority(AuthoritySet.MEMBER);
 
-    if (i <= 5) { //1번부터 5번까지의 회원은 member권한과 admin권한을 둘 다 갖도록...
-      member.addAuthority(AuthoritySet.ADMIN);
-    }
+  //   if (i <= 5) { //1번부터 5번까지의 회원은 member권한과 admin권한을 둘 다 갖도록...
+  //     member.addAuthority(AuthoritySet.ADMIN);
+  //   }
 
-    memberRepository.save(member);
+  //   memberRepository.save(member);
 
-    });
-  }
+  //   });
+  // }
 }
