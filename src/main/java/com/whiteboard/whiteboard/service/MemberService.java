b56159beaa4816256c915b01d71356d329d951da
한울 <bluemoon_sound@naver.com>
2023-09-15 14:22:59 +0900

    중간 푸쉬

--- src/main/java/com/whiteboard/whiteboard/service/MemberService.java
+++ src/main/java/com/whiteboard/whiteboard/service/MemberService.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Service;
 
 import com.whiteboard.whiteboard.dto.MemberDTO;
 
-@Service
+// 회원 관리와 관련된 비즈니스 로직을 수행하는 서비스
+// 회원 가입, 로그인, 회원 정보 조회 및 수정 등과 같은 회원 관련 기능을 제공
+// 주로 데이터베이스 CRUD(Create, Read, Update, Delete) 작업을 처리
 
+@Service
 public interface MemberService {
     
     //신규 회원 가입 메서드
