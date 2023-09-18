e3db83ff8e9f4fef5defece3f90d25f78deb9cc6
고니 <chansol91@naver.com>
2023-09-18 16:37:20 +0900

    수정

--- src/main/java/com/whiteboard/whiteboard/service/MemberServiceImpl.java
+++ src/main/java/com/whiteboard/whiteboard/service/MemberServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.whiteboard.whiteboard.dto.MemberDTO;
 import com.whiteboard.whiteboard.entity.Member;
 import com.whiteboard.whiteboard.repository.MemberRepository;
 
@@ -16,23 +15,23 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-    //private final PasswordEncoder passwordEncoder;
+    // private final PasswordEncoder passwordEncoder;
 
     // 회원가입
     @Override
-    public String memberRegister(MemberDTO memberDTO) {
+    public String memberRegister(Member member) {
 
-        Member member = Member.builder()
-                .email(memberDTO.getEmail())
-                .pw(memberDTO.getPw())
-                .phoneNum(memberDTO.getPhoneNum())
-                .nickname(memberDTO.getNickname())
-                .gender(memberDTO.getGender())
-                .birthDay(memberDTO.getBirthDay())
-                .name(memberDTO.getName())
-                .isSns(memberDTO.isSns())
-                .build();
-            // 회원 정보를 저장        
+        // Member member = Member.builder()
+        //         .email(memberDTO.getEmail())
+        //         .pw(memberDTO.getPw())
+        //         .phoneNum(memberDTO.getPhoneNum())
+        //         .nickname(memberDTO.getNickname())
+        //         .gender(memberDTO.getGender())
+        //         .birthDay(memberDTO.getBirthDay())
+        //         .name(memberDTO.getName())
+        //         .isSns(memberDTO.isSns())
+        //         .build();
+        // 회원 정보를 저장
         memberRepository.save(member);
 
         return member.getEmail();
@@ -50,17 +49,40 @@ public class MemberServiceImpl implements MemberService {
         memberRepository.findByEmail(email);
     }
 
-    public Optional<Member> findOne(String email){
-        return memberRepository.findByEmail(email);
-    }
-    //이메일 중복 검사 TEST
-	@Override
-	public boolean isEmailAlreadyExists(String email) {
+    // 이메일 중복 검사 메서드
+    @Override
+    public boolean isEmailRegistered(String email) {
         Optional<Member> existingMember = memberRepository.findByEmail(email);
-        return existingMember.isPresent();
-	}
+        // 이미 존재하는 이메일인 경우
+        if (existingMember.isPresent()) {
+            return true; // 중복된 이메일이 있음을 나타내는 true 반환
+        } else {
+            return false; // 중복된 이메일이 없음을 나타내는 false 반환
+        }
+    }
 
+    // 전화번호 중복 검사 메서드
+    @Override
+    public boolean isPhoneNumberRegistered(String phoneNum) {
+        Optional<Member> existingMember = memberRepository.findByphoneNum(phoneNum);
+        // 이미 존재하는 전화번호인 경우
+        if (existingMember.isPresent()) {
+            return true; // 중복된 전화번호가 있음을 나타내는 true 반환
+        } else {
+            return false; // 중복된 전화번호가 없음을 나타내는 false 반환
+        }
+    }
 
-    
+    // 닉네임 중복 검사 메서드
+    @Override
+    public boolean isNicknameRegistered(String nickname) {
+        Optional<Member> existingMember = memberRepository.findBynickname(nickname);
+        // 이미 존재하는 닉네임인 경우
+        if (existingMember.isPresent()) {
+            return true; // 중복된 닉네임이 있음을 나타내는 true 반환
+        } else {
+            return false; // 중복된 닉네임이 없음을 나타내는 false 반환
+        }
+    }
 
 }
