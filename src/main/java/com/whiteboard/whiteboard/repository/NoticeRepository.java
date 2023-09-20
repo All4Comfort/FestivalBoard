a3cb4c38d4b1e76e2b4052986c524429bea87af6
송아현 <dkgus810@naver.com>
2023-09-20 16:16:58 +0900

    아현

--- src/main/java/com/whiteboard/whiteboard/repository/NoticeRepository.java
+++ src/main/java/com/whiteboard/whiteboard/repository/NoticeRepository.java
@@ -1,9 +1,20 @@
 package com.whiteboard.whiteboard.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.whiteboard.whiteboard.entity.Notice;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-   
+
+@Query("SELECT n, m FROM Notice n LEFT JOIN n.writer m WHERE n.noticeNum = :noticeNum")
+Object getNoticeWithWriter(@Param("noticeNum") Long noticeNum);
+
+@Query("SELECT n, m FROM Notice n JOIN n.writer m")
+Page<Object[]> getNoticeWithWriter(Pageable pageable);
+
+
 }
