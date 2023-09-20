80d8e8dbc86056cf30471e468b042e2c1b306f73
고니 <chansol91@naver.com>
2023-09-20 11:33:39 +0900

    고니고니

--- src/main/java/com/whiteboard/whiteboard/repository/FestivalRepository.java
+++ src/main/java/com/whiteboard/whiteboard/repository/FestivalRepository.java
@@ -11,12 +11,14 @@ import com.whiteboard.whiteboard.entity.Festival;
 @Repository
 public interface FestivalRepository extends JpaRepository<Festival, Long> {
 
-	 @Query(value = "SELECT * FROM festival ORDER BY RAND() LIMIT 5", nativeQuery = true)
+    
+    // 타이틀,포스터 5개 랜덤으로 가져오기
+    @Query(value = "SELECT festival_title  AS festival_title , poster FROM festival ORDER BY RAND() LIMIT 5", nativeQuery = true)
     List<Festival> findRandomFestival();
 
     // @Modifying
-    // @Query("update Festival e set e.read_count = e.read_count + 1 where e.festival_num = :festival_num")
+    // @Query("update Festival e set e.read_count = e.read_count + 1 where
+    // e.festival_num = :festival_num")
     // int updateReadCount(Long festivalNum);
 
-    
 }
