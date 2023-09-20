e9691171345f0eb0cf92e0bd18b6f6b64466b086
송아현 <dkgus810@naver.com>
2023-09-20 14:13:25 +0900

    아현 공지

---
+++ src/main/java/com/whiteboard/whiteboard/controller/NoticeController.java
@@ -0,0 +1,25 @@
+package com.whiteboard.whiteboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.whiteboard.whiteboard.dto.PageRequestDTO;
+import com.whiteboard.whiteboard.service.NoticeService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+
+public class NoticeController {
+  private final NoticeService noticeService;
+
+
+  @GetMapping("/notice1")
+  public String notice(PageRequestDTO pageRequestDTO, Model model){
+    //model.addAttribute("result", noticeService.getList(pageRequestDTO));
+    return "/notice1";
+  }
+
+}
--- src/main/java/com/whiteboard/whiteboard/service/NoticeServiceImpl.java
+++ src/main/java/com/whiteboard/whiteboard/service/NoticeServiceImpl.java
@@ -2,10 +2,24 @@ package com.whiteboard.whiteboard.service;
 
 import org.springframework.stereotype.Service;
 
+import com.whiteboard.whiteboard.dto.NoticeDTO;
+import com.whiteboard.whiteboard.dto.PageRequestDTO;
+import com.whiteboard.whiteboard.dto.PageResultDTO;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class NoticeServiceImpl implements NoticeService{
+public class NoticeServiceImpl implements NoticeService{@Override
+  public Long register(NoticeDTO dto) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'register'");
+  }
+
+@Override
+public PageResultDTO<NoticeDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getList'");
+}
     
 }
