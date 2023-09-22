8f2e630c01f9555b28a5626fa60cbb3c20866d3a
고니 <chansol91@naver.com>
2023-09-22 15:49:25 +0900

    메인 썸네일 넣기

--- src/main/java/com/whiteboard/whiteboard/controller/FestivalController.java
+++ src/main/java/com/whiteboard/whiteboard/controller/FestivalController.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.whiteboard.whiteboard.dto.FestivalDTO;
 import com.whiteboard.whiteboard.service.FestivalService;
@@ -14,34 +14,47 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-//@RequestMapping("/member")
+// @RequestMapping("/member")
 public class FestivalController {
 
     private final FestivalService festivalService;
+    //밑 코드는 모듈에 메시지 넣기위해 필요해서 넣었음
+    // private final Logger logger = LoggerFactory.getLogger(FestivalController.class);
 
-    //한울쓰
+    // 한울쓰//한울쓰~~ 이건 모듈에 반환하는거라 에러 났었데 그래서 지웁니다
+    // @GetMapping("/member/main")
+    // public void getFestivals(Model model) {
+
+    //     // 모델에 데이터 추가
+    //     List<FestivalDTO> festivals = festivalService.getFiveDTOs();
+    //     logger.info("축제 목록: {}", festivals);
+
+    //     // 모델에 데이터 추가
+    //     model.addAttribute("festivals", festivalService.getFiveDTOs());
+
+    // }
+
     @GetMapping("/member/main")
-    public void getFestivals(Model model) {
-        
-
-        // 모델에 데이터 추가
-        model.addAttribute("festivals", festivalService.getFiveDTOs());
-
+    @ResponseBody // 이 어노테이션을 추가하여 메서드가 JSON 데이터를 반환함을 명시합니다.
+    public List<FestivalDTO> getFestivals() {
+        List<FestivalDTO> festivals = festivalService.getFiveDTOs();
+       // logger.info("축제 목록: {}", festivals); //콘솔에 찍히나 확인했습니다.
+        return festivals; // JSON 형식의 데이터를 반환합니다.
     }
 
     // 3번째 방법
-    /* 
-    @GetMapping("/test")
-    public String getFestivals(Model model) {
-        // 랜덤한 축제 목록 생성 (실제로는 서비스 레이어를 통해 데이터를 가져옵니다)
-        List<FestivalDTO> festivals = createRandomFestivals();
-
-        // 모델에 데이터 추가
-        model.addAttribute("festivals", festivals);
-
-        return "test"; // 타임리프 템플릿 이름
-    }
-    */
+    /*
+     * @GetMapping("/test")
+     * public String getFestivals(Model model) {
+     * // 랜덤한 축제 목록 생성 (실제로는 서비스 레이어를 통해 데이터를 가져옵니다)
+     * List<FestivalDTO> festivals = createRandomFestivals();
+     * 
+     * // 모델에 데이터 추가
+     * model.addAttribute("festivals", festivals);
+     * 
+     * return "test"; // 타임리프 템플릿 이름
+     * }
+     */
 
     // 실제 데이터를 가져오는 로직은 이곳에서 수행해야 합니다.
     private List<FestivalDTO> createRandomFestivals() {
