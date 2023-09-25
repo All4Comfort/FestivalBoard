2f5c48ae07033759b232b8f9324da28dc0c0bca7
고니 <chansol91@naver.com>
2023-09-25 17:31:23 +0900

    게시판

--- src/main/java/com/whiteboard/whiteboard/service/FestivalService.java
+++ src/main/java/com/whiteboard/whiteboard/service/FestivalService.java
@@ -2,40 +2,43 @@ package com.whiteboard.whiteboard.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.whiteboard.whiteboard.dto.FestivalDTO;
 import com.whiteboard.whiteboard.entity.Festival;
 
 public interface FestivalService {
 
-      //한울쓰
+      // 한울쓰
       List<FestivalDTO> getFiveDTOs();
 
-  
+      public List<FestivalDTO> getAllFestivalsAsDTO();
 
-       public List<FestivalDTO> getAllFestivalsAsDTO();
-
-         //미건쓰: 게시물 메서드 정의
+      // 미건쓰: 게시물 메서드 정의
       List<FestivalDTO> findAllByOrderByFestivalNumAsc();
 
-       //Entity를 DTO로 변환하는 메서드 선언
-	default FestivalDTO entityToDTO(Festival festival) {
-		
-		FestivalDTO dto = FestivalDTO.builder()
-                              .festivalNum(festival.getFestivalNum())
-					.festivalTitle(festival.getFestivalTitle())
-					.region(festival.getRegion())
-                              .venue(festival.getVenue())
-                              .period(festival.getPeriod())
-                              .description(festival.getDescription())
-                              .link(festival.getLink())
-                              .poster(festival.getPoster())
-                              .thumbnail(festival.getThumbnail())
-                              .registerDate(festival.getRegisterDate())
-                              .modifyDate(festival.getModifyDate())
-                              
-					.build(); 
-		return dto;
-	}
-   
-   
+      // 축제 페이징
+      Page<FestivalDTO> findAllByOrderByFestivalNum(Pageable pageable);
+
+      // Entity를 DTO로 변환하는 메서드 선언
+      default FestivalDTO entityToDTO(Festival festival) {
+
+            FestivalDTO dto = FestivalDTO.builder()
+                        .festivalNum(festival.getFestivalNum())
+                        .festivalTitle(festival.getFestivalTitle())
+                        .region(festival.getRegion())
+                        .venue(festival.getVenue())
+                        .period(festival.getPeriod())
+                        .description(festival.getDescription())
+                        .link(festival.getLink())
+                        .poster(festival.getPoster())
+                        .thumbnail(festival.getThumbnail())
+                        .registerDate(festival.getRegisterDate())
+                        .modifyDate(festival.getModifyDate())
+
+                        .build();
+            return dto;
+      }
+
 }
