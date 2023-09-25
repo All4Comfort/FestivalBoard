4d9f74c3e7fcc945fef52d1c825ee601cbc26998
오아연 <dkdus555@naver.com>
2023-09-25 19:28:56 +0900

    review

--- src/main/java/com/whiteboard/whiteboard/controller/ReviewController.java
+++ src/main/java/com/whiteboard/whiteboard/controller/ReviewController.java
@@ -1,17 +1,15 @@
 package com.whiteboard.whiteboard.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.whiteboard.whiteboard.dto.PageRequestDTO;
 import com.whiteboard.whiteboard.dto.ReviewDTO;
-import com.whiteboard.whiteboard.entity.Review;
 import com.whiteboard.whiteboard.repository.ReviewRepository;
 import com.whiteboard.whiteboard.service.ReviewService;
 
@@ -23,31 +21,49 @@ public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
- 
-  @GetMapping("/reviewList")
-    public String getReviews() {
+
+    @GetMapping("/reviewList")
+    public String getReviews(PageRequestDTO pageRequestDTO, Model model) {
+        //model.addAttribute("result", reviewRepository.getReviewNum(1L));
+        //model.addAttribute("result", reviewRepository.getReviewJoinMember(1L));
+        model.addAttribute("result", reviewRepository.getReviewList());
+
+        
+        //List<Review[]> reviewList = reviewRepository.getReviewList();
+
+        //model.addAttribute("result", reviewList);
+
         // 여기에 리뷰 목록을 가져오는 로직을 추가하세요.
         // 가상의 데이터를 사용하는 경우, 리스트를 생성하고 반환하면 됩니다.
-        
-        List<Review> reviews = new ArrayList<>();
+
+        // List<Review> reviews = new ArrayList<>();
         // 리뷰 데이터를 가져와서 reviews 리스트에 추가
-
-        return "/reviewList";
+        return "reviewList";
     }
-
+    
     @GetMapping("/reviewDetail")
-    public String getReviewDetail(@PathVariable Long id, Model model){
-        ReviewDTO reviewDTO = reviewService.getReviewById(id);
-
-        model.addAttribute("review", reviewDTO);
-
+    public String getReviewDetail(@ModelAttribute ReviewDTO reviewDTO, Model model) {
+        //ReviewDTO reviewDTO = reviewService.getReviewById(reviewNumLong);
+        //model.addAttribute("result", reviewRepository.getReviewList());
+       // model.addAttribute("result", reviewRepository.getReviewNum(1L));
+        model.addAttribute("result", reviewService.getNickname());
+        
         return "/reviewDetail";
     }
 
-    @GetMapping("/reviewWrite")
-    public String postWriteReview(@ModelAttribute ReviewDTO reviewDTO) {
-        // 리뷰 작성 페이지로 이동
-    return "/reviewWrite"; // 작성할 리뷰 페이지의 경로로 리다이렉트
-    }
+    
 
+@GetMapping("/reviewWrite")
+public void postWriteReview() {
+    
+}
+
+    @PostMapping("/reviewWrite")
+    public String postWriteReview(ReviewDTO dto, RedirectAttributes attributes) {
+        
+        Long reLong = reviewService.saveReview(dto, null);
+
+        attributes.addFlashAttribute("reLong", reLong);
+        return "redirect:/reviewWrite"; // 작성할 리뷰 페이지의 경로로 리다이렉트
+    }
 }
