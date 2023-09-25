6080d67bebe0df0ddd75aa5fc51a661ed7a1cc5d
한울 <bluemoon_sound@naver.com>
2023-09-25 21:33:11 +0900

    수정

--- src/main/java/com/whiteboard/whiteboard/service/FestivalReplyService.java
+++ src/main/java/com/whiteboard/whiteboard/service/FestivalReplyService.java
@@ -2,23 +2,69 @@ package com.whiteboard.whiteboard.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.whiteboard.whiteboard.dto.ReplyDTO;
 import com.whiteboard.whiteboard.entity.Festival;
 import com.whiteboard.whiteboard.entity.FestivalReply;
+import com.whiteboard.whiteboard.entity.Member;
+import com.whiteboard.whiteboard.repository.MemberRepository;
 
-public interface FestivalReplyService {
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class FestivalReplyService {
 
-  List<ReplyDTO> getAllReply(Long festivalNum);
-  
-  List<ReplyDTO> register(ReplyDTO replyDTO);
+  public final MemberRepository memberRepository;
 
-  //댓글 수정 메서드 선언
-	void modify(ReplyDTO dto);
+  public List<ReplyDTO> getAllReply(Long festivalNum) {
+        return null;
+  }
+
+  public List<ReplyDTO> register(ReplyDTO replyDTO) {
+        return null;
+  }
+
+  public void modify(ReplyDTO dto) {
+
+  }
+
+  public void remove(Long rno) {
+    
+  }
+  //DTO를 Entity로 변환하는 메서드 선언
+	public FestivalReply dtoToEntity(ReplyDTO replyDTO) {
+		
+		//글번호가 필요하니 Festival Entity를 참조해야 한다.
+    Festival festival = Festival.builder().festivalNum(replyDTO.getFestivalNum()).build();
+		
+		Member member = memberRepository.getReferenceById(replyDTO.getWriter());
+
+    FestivalReply festivalReply = FestivalReply.builder()
+															.from(festival)
+															.content(replyDTO.getContent())
+															.replyLevel(replyDTO.getReplyLevel())
+															.replyStep(replyDTO.getReplyStep())
+															.writer(member)
+															.build();
+		
+		return festivalReply;
+	}
 	
-	//댓글 삭제 메서드 선언
-	void remove(Long rno); 
-
-  
-	
+	//Entity를 DTO로 변환하는 메서드 선언
+	public ReplyDTO entityToDTO(FestivalReply reply) {
+		
+		ReplyDTO dto = ReplyDTO.builder()
+					    .writer(reply.getWriter().getEmail())
+              .content(reply.getContent())
+              .replyLevel(reply.getReplyLevel())
+              .replyStep(reply.getReplyStep())
+              .festivalNum(reply.getFrom().getFestivalNum())
+              .regDate(reply.getRegisterDate())
+              .modDate(reply.getModifyDate())
+					    .build();
+		
+		return dto;
+	}
 }
