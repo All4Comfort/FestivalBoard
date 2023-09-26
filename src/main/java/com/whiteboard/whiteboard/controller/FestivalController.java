232452f2146116889ff90556bd06481525ad1f64
고니 <chansol91@naver.com>
2023-09-26 18:58:26 +0900

    축제 페이징 완료

--- src/main/java/com/whiteboard/whiteboard/controller/FestivalController.java
+++ src/main/java/com/whiteboard/whiteboard/controller/FestivalController.java
@@ -42,41 +42,44 @@ public class FestivalController {
     // 전체 죽제 목록을 가져오기
     // @GetMapping("/festival/festivalList")
     // public String showFestivalList(PageRequestDTO pageRequestDTO, Model model) {
-    //     List<FestivalDTO> festivals = festivalService.findAllByOrderByFestivalNumAsc();
-    //     // System.out.println("축제 목록: " + festivals);
-    //     model.addAttribute("festivals", festivals);
-    //     return "/festival/festivalList";
+    // List<FestivalDTO> festivals =
+    // festivalService.findAllByOrderByFestivalNumAsc();
+    // // System.out.println("축제 목록: " + festivals);
+    // model.addAttribute("festivals", festivals);
+    // return "/festival/festivalList";
     // }
 
-    //페이징을 위해
+    // 페이징을 위해
     @GetMapping("/festival/festivalList")
-public String showFestivalList(PageRequestDTO pageRequestDTO, Model model) {
-    Pageable pageable = pageRequestDTO.getPageable(Sort.by("festivalNum").ascending());
-    Page<FestivalDTO> festivals = festivalService.findAllByOrderByFestivalNum(pageable);
-    model.addAttribute("festivals", festivals);
+    public String showFestivalList(PageRequestDTO pageRequestDTO, Model model) {
+        Pageable pageable = pageRequestDTO.getPageable(Sort.by("festivalNum").ascending());
+        Page<FestivalDTO> festivals = festivalService.findAllByOrderByFestivalNum(pageable);
 
-    // 총 페이지 수 계산
-    int totalPages = festivals.getTotalPages();
+        int totalPages = festivals.getTotalPages();
+        int currentPage = pageRequestDTO.getPage();
 
-    // 현재 페이지 번호 가져오기
-    int currentPage = pageRequestDTO.getPage();
+        // 페이지 번호 목록 생성 (고정된 3개 페이지만 표시)
+        List<Integer> pageNumbers = IntStream
+                .rangeClosed(Math.max(1, currentPage - 1), Math.min(currentPage + 1, totalPages))
+                .boxed()
+                .collect(Collectors.toList());
 
-    // 페이지 번호 목록 생성
-    List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-            .boxed()
-            .collect(Collectors.toList());
+        boolean hasPrevPage = currentPage > 1;
+        boolean hasNextPage = currentPage < totalPages;
 
-    // 이전 페이지 및 다음 페이지 버튼 처리
-    boolean hasPrevPage = currentPage > 1;
-    boolean hasNextPage = currentPage < totalPages;
+        int prevPageNumber = currentPage - 1;
+        int nextPageNumber = currentPage + 1;
 
-    model.addAttribute("pageNumbers", pageNumbers);
-    model.addAttribute("hasPrevPage", hasPrevPage);
-    model.addAttribute("hasNextPage", hasNextPage);
+        model.addAttribute("festivals", festivals);
+        model.addAttribute("pageNumbers", pageNumbers);
+        model.addAttribute("hasPrevPage", hasPrevPage);
+        model.addAttribute("hasNextPage", hasNextPage);
+        model.addAttribute("prevPageNumber", prevPageNumber);
+        model.addAttribute("nextPageNumber", nextPageNumber);
 
-    return "/festival/festivalList";
-}
-    
+        return "/festival/festivalList";
+    }
+
     // 축제 상세페이지 넘기기
     @GetMapping("/festival/festivalDetail")
     public String getFestivalDetail(@RequestParam("festivalNum") Long festivalNum, Model model) {
