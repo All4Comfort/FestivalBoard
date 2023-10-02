9caceb5fafea21f249cedefcc8b2fcf26ab250b3
한울 <bluemoon_sound@naver.com>
2023-10-02 10:39:40 +0900

    리플라이

--- src/main/java/com/whiteboard/whiteboard/controller/ReplyController.java
+++ src/main/java/com/whiteboard/whiteboard/controller/ReplyController.java
@@ -1,8 +1,13 @@
 package com.whiteboard.whiteboard.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.whiteboard.whiteboard.dto.ReplyDTO;
 import com.whiteboard.whiteboard.service.FestivalReplyService;
+import com.whiteboard.whiteboard.service.ReviewReplyService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,24 +16,37 @@ import lombok.RequiredArgsConstructor;
 public class ReplyController {
 
     private final FestivalReplyService festivalReplyService;
-/*
+    private final ReviewReplyService reviewReplyService;
+    
+    // ResponseEntity는 Spring Framework에서 사용되는 클래스로, HTTP 응답을 생성하고 반환하는 데 사용
+    // 클라이언트에게 HTTP 응답을 보내는 데 필요한 모든 정보를 포함
+    // 응답의 상태 코드와 내용을 설정할 수 있어 클라이언트에게 적절한 응답을 보내는 데 유용
+    // HTTP 상태 코드, 응답 본문, 헤더 등을 설정할 수 있는 방법을 제공
+
     //댓글 등록하는 Post 메서드 정의
     @PostMapping("/festival/save")
     public ResponseEntity saveFestivalReply(@ModelAttribute ReplyDTO replyDTO) {
         System.out.println("replyDTO = " + replyDTO);
-        Long saveResult = festivalReplyService.register(replyDTO);
+        Long saveResult = reviewReplyService.save(replyDTO);
+        if (saveResult != null) {
+    //         List<ReplyDTO> replyDTOList = festivalReplyService.findAll(replyDTO.getReviewNum());
+    //         return new ResponseEntity<>(replyDTOList, HttpStatus.OK);
+    //     } else {
+    //         return new ResponseEntity<>("해당 게시글이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+         }
+         return new ResponseEntity(null);
     }
 
-    @PostMapping("/save")
-    public ResponseEntity save(@ModelAttribute CommentDTO commentDTO) {
-        System.out.println("commentDTO = " + commentDTO);
-        Long saveResult = commentService.save(commentDTO);
-        if (saveResult != null) {
-            List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
-            return new ResponseEntity<>(commentDTOList, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("해당 게시글이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
-        }
-    }
- */
+    // @PostMapping("/save")
+    // public ResponseEntity save(@ModelAttribute CommentDTO commentDTO) {
+    //     System.out.println("commentDTO = " + commentDTO);
+    //     Long saveResult = commentService.save(commentDTO);
+    //     if (saveResult != null) {
+    //         List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
+    //         return new ResponseEntity<>(commentDTOList, HttpStatus.OK);
+    //     } else {
+    //         return new ResponseEntity<>("해당 게시글이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+    //     }
+    // }
+ 
 }
--- src/main/java/com/whiteboard/whiteboard/service/ReviewReplyService.java
+++ src/main/java/com/whiteboard/whiteboard/service/ReviewReplyService.java
@@ -1,5 +1,56 @@
 package com.whiteboard.whiteboard.service;
 
-public interface ReviewReplyService {
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.whiteboard.whiteboard.dto.ReplyDTO;
+import com.whiteboard.whiteboard.entity.Review;
+import com.whiteboard.whiteboard.repository.ReviewReplyRepository;
+import com.whiteboard.whiteboard.repository.ReviewRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewReplyService {
+
+  private final ReviewRepository reviewRepository;
+  private final ReviewReplyRepository reviewReplyRepository;
+
+  public Long save(ReplyDTO replyDTO) {
+
+      //부모엔티티(Review) 조회
+      Optional<Review> optionalReviewEntity = reviewRepository.findById(replyDTO.getReviewNum());
+      if(optionalReviewEntity.isPresent()) {
+        Review review = optionalReviewEntity.get();
+        //FestivalReply festivalReplyEntity = 
+      }
+      return null;
+  }
+    
+  // public Long save(CommentDTO commentDTO) {
+  //       /* 부모엔티티(BoardEntity) 조회 */
+  //       Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(commentDTO.getBoardId());
+  //       if (optionalBoardEntity.isPresent()) {
+  //           BoardEntity boardEntity = optionalBoardEntity.get();
+  //           CommentEntity commentEntity = CommentEntity.toSaveEntity(commentDTO, boardEntity);
+  //           return commentRepository.save(commentEntity).getId();
+  //       } else {
+  //           return null;
+  //       }
+  //   }
+
+  //   public List<CommentDTO> findAll(Long boardId) {
+  //       BoardEntity boardEntity = boardRepository.findById(boardId).get();
+  //       List<CommentEntity> commentEntityList = commentRepository.findAllByBoardEntityOrderByIdDesc(boardEntity);
+  //       /* EntityList -> DTOList */
+  //       List<CommentDTO> commentDTOList = new ArrayList<>();
+  //       for (CommentEntity commentEntity: commentEntityList) {
+  //           CommentDTO commentDTO = CommentDTO.toCommentDTO(commentEntity, boardId);
+  //           commentDTOList.add(commentDTO);
+  //       }
+  //       return commentDTOList;
+  //   }
 }
+
